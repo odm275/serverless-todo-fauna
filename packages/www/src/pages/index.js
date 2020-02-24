98d805de87da031a5f9bdd4e39fb74db24b71f9f
odm275 <pomejia@gmail.com>
2020-02-24 11:04:13 -0600

    gatsby starter w theme ui

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Container, Heading, Button, Flex } from "theme-ui"
 export default props => (
-  <div>
-    <h1>Hiiiiii</h1>
-  </div>
+  <Container>
+    <Flex sx={{flexDirection: "column", padding: 3}}>
+      <Heading as="h1">Get stuff done</Heading>
+      <Button onClick={() => {
+        alert("clicked")
+      }}>Log In</Button>
+    </Flex>
+  </Container>
 );
