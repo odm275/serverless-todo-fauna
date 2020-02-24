68f7e6d79b062c2271d086e948c5119d75babbaf
odm275 <pomejia@gmail.com>
2020-02-24 11:34:08 -0600

    auth widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { Container, Heading, Button, Flex } from "theme-ui"
-export default props => (
-  <Container>
-    <Flex sx={{flexDirection: "column", padding: 3}}>
-      <Heading as="h1">Get stuff done</Heading>
-      <Button onClick={() => {
-        alert("clicked")
-      }}>Log In</Button>
-    </Flex>
-  </Container>
-);
+import { React, useEffect } from "react";
+import { Container, Heading, Button, Flex } from "theme-ui";
+import netlifyIdentity from "netlify-identity-widget";
+
+netlifyIdentity.init({});
+
+export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{flexDirection: "column", padding: 3}}>
+        <Heading as="h1">Get stuff done</Heading>
+        <Button
+          sx={{ marginTop: 2 }} 
+          onClick={() => {
+          netlifyIdentity.open();
+        }}>
+          Log In
+        </Button>
+      </Flex>
+    </Container>
+  );
+}
